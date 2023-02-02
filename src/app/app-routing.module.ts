fffc01ffd7b6cbb8ea707c4c1b51bfc2b5ed329d
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-02-02 19:50:40 +0400

    Finished Naruto component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimesComponent } from './views/animes/animes.component';
 import { AllAnimeComponent } from './views/all-anime/all-anime.component';
+import { NarutoComponent } from './views/naruto/naruto.component';
+import { OnePieceComponent } from './views/one-piece/one-piece.component';
+import { BleachComponent } from './views/bleach/bleach.component';
 
 const routes: Routes = [
   { path: "", component: AnimesComponent},
-  // { path: "allAnime", component: AllAnimeComponent}  
+  { path: "naruto", component: NarutoComponent}  ,
+  {path: "one-piece", component: OnePieceComponent},
+  {path: "bleach", component: BleachComponent}
 ];
 
 @NgModule({
--- src/app/views/naruto/naruto.component.ts
+++ src/app/views/naruto/naruto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AnimeService } from 'src/app/shared_services/anime.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-naruto',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./naruto.component.css']
 })
 export class NarutoComponent implements OnInit {
+  allAnimeList:any [] = [];
+  apiUrl:any = "https://api.jikan.moe/v4/anime?q=naruto&sfw";
 
-  constructor() { }
+  constructor(private router:Router, 
+    private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getNarutoData().subscribe((response:any) => {
+      this.allAnimeList = response.data;
+    })
+  }
+
+  getNarutoData():Observable<any> {
+    return this.httpClient.get(this.apiUrl);
   }
 
 }
